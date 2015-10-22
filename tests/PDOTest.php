88dba9b99bcd7090657ba78ec42cb10ac786ad8e
Simas Godovan <simas@godovan.lt>
2015-10-22 23:53:09 +0300

    Add @covers annotation to test how coverage report changes.

--- tests/PDOTest.php
+++ tests/PDOTest.php
@@ -4,6 +4,9 @@ use PreSQL\PDO;
 
 class PDOTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers \PreSQL\PDO
+     */
     public function testIsPreSQLPDOVersionReturnedCorrectly()
     {
         $this->assertEquals(PDO::getVersion(), PDO::$version);
